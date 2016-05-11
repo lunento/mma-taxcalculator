72a3e8894fd0032f858621c5529dfce27f0fb813
Ernie Barojas <ernie@ERNIEs-Mac-Pro.local>
2016-05-11 13:26:23 -0700

    first commit

---
+++ TaxCalculatorApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaxCalculatorApp
+//
+//  Created by Ernie Barojas on 5/11/16.
+//  Copyright © 2016 Ernie Barojas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TaxCalculatorApp/ViewController.m
@@ -0,0 +1,52 @@
+//
+//  ViewController.m
+//  TaxCalculatorApp
+//
+//  Created by Ernie Barojas on 5/11/16.
+//  Copyright © 2016 Ernie Barojas. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *priceTextField;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
+@property double sfTax;
+@property double chiTax;
+@property double nyTax;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.sfTax = .0875;
+    self.chiTax = .1025;
+    self.nyTax = .08875;
+    
+    
+}
+
+
+- (IBAction)onCalculateButtonTapped:(UIButton *)sender {
+    
+    NSString *enteredText = self.priceTextField.text;
+    int enteredValue = enteredText.intValue;
+    
+    if (self.segmentedControl.selectedSegmentIndex == 0) {
+        double result = enteredValue * self.sfTax;
+        self.resultLabel.text = [NSString stringWithFormat:@"$%.2f", result];
+    } else if (self.segmentedControl.selectedSegmentIndex == 1) {
+        double result = enteredValue * self.chiTax;
+        self.resultLabel.text = [NSString stringWithFormat:@"$%.2f", result];
+    } else {
+        double result = enteredValue * self.nyTax;
+        self.resultLabel.text = [NSString stringWithFormat:@"$%.2f", result];
+    }
+}
+@end
